b562f904031e8de918ff1fe7c43d68a9c7bfbf46
Diego García <diegogarcia@MacBook-Pro.local>
2021-01-12 11:15:30 +0100

    Add refresh token

--- src/Swagger/Decorator.php
+++ src/Swagger/Decorator.php
@@ -42,7 +42,7 @@ final class Decorator implements OpenApiFactoryInterface
                                                       ],
                                                   ]);
 
-        $pathItem = new Model\PathItem(
+        $jwtPath = new Model\PathItem(
             ref: 'JWT Token',
             post: new Model\Operation(
                      operationId: 'postCredentialsItem',
@@ -71,7 +71,49 @@ final class Decorator implements OpenApiFactoryInterface
                                   ),
                  ),
         );
-        $openApi->getPaths()->addPath('/authentication_token', $pathItem);
+        $openApi->getPaths()->addPath('/authentication_token', $jwtPath);
+
+        $schemas['RefreshToken'] = new ArrayObject([
+                                                      'type' => 'object',
+                                                      'properties' => [
+                                                          'refresh_token' => [
+                                                              'type' => 'string',
+                                                              'example' => 'xxxx4b54b0076d2fcc5a51a6e60c0fb83b0bc90b47e2c886accb70850795fb311973c9d101fa0111f12eec739db063ec09d7dd79331e3148f5fc6e9cb362xxxx',
+                                                          ],
+                                                      ],
+                                                  ]);
+
+        $refreshTokenPath = new Model\PathItem(
+            ref: 'Refresh Token',
+            post: new Model\Operation(
+                     operationId: 'postRefreshTokenItem',
+                     responses: [
+                                      '200' => [
+                                          'description' => 'Refresh the token',
+                                          'content' => [
+                                              'application/json' => [
+                                                  'schema' => [
+                                                      '$ref' => '#/components/schemas/Token',
+                                                  ],
+                                              ],
+                                          ],
+                                      ],
+                                  ],
+                     summary: 'Refresh the token.',
+                     requestBody: new Model\RequestBody(
+                                      description: 'Generate new JWT Token',
+                                      content: new ArrayObject([
+                                                                   'application/json' => [
+                                                                       'schema' => [
+                                                                           '$ref' => '#/components/schemas/Credentials',
+                                                                       ],
+                                                                   ],
+                                                               ]),
+                                  ),
+                 ),
+        );
+
+        $openApi->getPaths()->addPath('/refresh_token', $refreshTokenPath);
 
         return $openApi;
     }
